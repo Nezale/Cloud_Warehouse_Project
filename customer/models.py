77eb74c3fc85ce329958fc42a63b487b0575e1c2
AleksanderKozyra <olek980@wp.pl>
2019-01-07 20:54:10 +0100

    added shopping cart

--- customer/models.py
+++ customer/models.py
@@ -15,7 +15,7 @@ class Customer(models.Model):
                                    )
     company_name = models.CharField(max_length=50, blank=True)
     orders = models.ManyToManyField('order.Order',
-                               blank=True)
+                                    blank=True)
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
