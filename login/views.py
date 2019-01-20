d7e258dcd5f32735b691fbd1d9dca7400519dcb6
Daniel Belter <idanielo850@gmail.com>
2019-01-20 17:31:21 +0100

    Add group check functionality

--- login/views.py
+++ login/views.py
@@ -5,13 +5,15 @@ from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.http import HttpResponse
+from meal.models import Meal
+from login.models import Raport
 
 
 # Create your views here.
 
+
 def main_login(request):
     form = AuthenticationForm()
-
     if request.method == 'POST':
         if 'LogInForm' in request.POST:
             form = AuthenticationForm(data=request.POST)
@@ -20,7 +22,8 @@ def main_login(request):
                 login(request, user)
                 user = User.objects.get(id=user.id)
                 if request.user.groups.filter(name='pracownik').exists():
-                        return render(request, 'pracownik.html', {'form': form})
+                    return render(request, 'pracownik.html', {'form': form, 'mealQuantity': Raport.meal_quantity})
+
                 return render(request, 'indexLogin.html', {'form': form})
             else:
                 return render(request, 'indexLogin.html', {'form': form})
@@ -31,9 +34,22 @@ def main_login(request):
 
     else:
         user = request.user.is_authenticated
+        if request.user.groups.filter(name='pracownik').exists():
+            return render(request, 'pracownik.html', {'form': form, 'mealQuantity': Raport.meal_quantity})
         if user:
             form = AuthenticationForm(data=request.POST)
             return render(request, 'indexLogin.html', {'form': form})
         else:
             form = AuthenticationForm()
             return render(request, 'indexLogin.html', {'form': form})
+
+
+def count_quantity(request):
+    Raport.meal_quantity = Meal.objects.get().quantity
+    return redirect('/login')
+
+
+def logout_p(request):
+    if request.method == 'POST':
+        logout(request)
+        return render(request, 'index.html')
